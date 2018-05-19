708bc7595a75234a74ab6bbbbb4d54cae13b9a3b
StephanRaab <stephan.raab@nyhart.com>
2018-05-19 17:50:53 -0400

    Variables

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 2;
+
+            b = a;
+            a = -3;
+            Console.WriteLine("a is " + a);
+            Console.WriteLine("b is " + b);
+            Console.ReadLine();
+        }
+    }
+}
