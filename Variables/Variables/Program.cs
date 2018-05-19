98735d3d5455d58a638ad719a684e6e90330251b
StephanRaab <stephan.raab@nyhart.com>
2018-05-19 17:55:12 -0400

    Good variable names

--- Variables/Variables/Program.cs
+++ Variables/Variables/Program.cs
@@ -10,6 +10,14 @@ namespace Variables
     {
         private static void Main(string[] args)
         {
+            //#1 Meet C# requirements
+            //#2 Variable names should descrive the stuff you INTEND on putting in it
+            //#3 Don't abbreviate or remove letters
+            //#4 A good name will usually be kind of long
+            //#5 If your variables end with a number, you probably need a better name
+            //#6 "data", "text", "number", and "item" are usually not descriptive enough
+            //#7 Make the words of the variable name stand out from each other
+
             int a = 5;
             int b = 2;
 
