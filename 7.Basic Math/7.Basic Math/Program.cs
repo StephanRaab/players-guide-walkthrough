b67c079712ebd74fd227c1464947eb29acdbf433
StephanRaab <stephan.raab@nyhart.com>
2018-05-19 19:30:21 -0400

    area of a triangle

--- 7.Basic Math/7.Basic Math/Program.cs
+++ 7.Basic Math/7.Basic Math/Program.cs
@@ -10,6 +10,7 @@ namespace _7.Basic_Math
     {
         private static void Main(string[] args)
         {
+            // Tipping Calculator
             float totalCost;
             float tipPercent;
             float tipAmount;
@@ -26,6 +27,14 @@ namespace _7.Basic_Math
 
             Console.WriteLine("Your tip total is: {0:N2}", tipAmount);
             Console.WriteLine("Your total with tip is: {0:N2}", totalWithTip);
+            Console.ReadLine();
+
+            // Area of a triangle
+            double area;
+            double areaBase = 1.5;
+            double areaHeight = 4;
+            area = 0.5 * areaBase * areaHeight;
+            Console.WriteLine("The area of a triangle with base " + areaBase + " and height " + areaHeight + " is " + area + ".");
             Console.ReadLine();
         }
     }
