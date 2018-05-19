7a216f154874bfa578d6f141ed0019b9fc418455
StephanRaab <stephan.raab@nyhart.com>
2018-05-19 19:24:47 -0400

    How much to tip? Practicing basic math operations

---
+++ 7.Basic Math/7.Basic Math/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.Basic_Math
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            float totalCost;
+            float tipPercent;
+            float tipAmount;
+            float totalWithTip;
+
+            Console.WriteLine("How much did it cost?");
+            totalCost = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("How much would you like to tip?");
+            tipPercent = float.Parse(Console.ReadLine());
+
+            tipAmount = totalCost * (tipPercent / 100);
+            totalWithTip = totalCost + tipAmount;
+
+            Console.WriteLine("Your tip total is: {0:N2}", tipAmount);
+            Console.WriteLine("Your total with tip is: {0:N2}", totalWithTip);
+            Console.ReadLine();
+        }
+    }
+}
