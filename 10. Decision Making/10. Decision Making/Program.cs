eec6c4a28b953bc12de6a4b0bbd098adfb4a0b16
StephanRaab <stephan.raab@nyhart.com>
2018-06-09 08:47:10 -0400

    * check if number is even or odd

---
+++ 10. Decision Making/10. Decision Making/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Decision_Making
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Input a random number:");
+            string randNum = Console.ReadLine();
+
+            if (Convert.ToInt32(randNum) % 2 == 0)
+            {
+                Console.WriteLine("I see, that's an even number!");
+            }
+            else
+            {
+                Console.WriteLine("That's an odd number in so many ways..");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
